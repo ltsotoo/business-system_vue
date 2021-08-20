ecceededb60fea40395f9c585cc55c5f15361d8a
ltsotoo <lts1311@outlook.com>
2021-08-20 17:40:23 +0800

    init

---
+++ src/components/area/Forms.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-card>
+    <v-card-title v-if="(openType = 0)">区域添加</v-card-title>
+    <v-card-title v-else>区域编辑</v-card-title>
+    <v-card-subtitle>
+      <v-form ref="form">
+        <v-row align="center">
+          <v-col cols="6">
+            <v-text-field
+              v-model.trim="object.name"
+              label="区域名称"
+              :rules="rules.name"
+            >
+            </v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-select
+              v-model="object.officeID"
+              :items="officeItems"
+              item-text="name"
+              item-value="ID"
+              label="办事处"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-form>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="submit"> 确定 </v-btn>
+        <v-btn color="blue darken-1" text @click="closeDialog"> 取消 </v-btn>
+      </v-card-actions>
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+import { queryOffices } from "@/api/oadrp";
+export default {
+  props: {
+    closeDialog: {
+      type: Function,
+    },
+    openType: {
+      type: Number,
+      default: 0,
+    },
+    parentObj: {
+      type: Object,
+    },
+  },
+  data: () => ({
+    officeItems: [],
+    object: {
+      name: "",
+      officeID: null,
+    },
+    rules: {
+      name: [
+        (v) => !!v || "必填项！",
+        (v) => (v && v.length < 20) || "名称的长度必须小于20个字符",
+      ],
+    },
+  }),
+  created() {
+    if (this.openType == 2) {
+      this.object = this.parentObj;
+    }
+    this.getOfficeItems();
+  },
+  methods: {
+    getOfficeItems() {
+      queryOffices().then((res) => {
+        this.officeItems = res.data;
+      });
+    },
+    submit() {
+      if (this.openType == 0) {
+      } else if (this.openType == 2) {
+      }
+    },
+  },
+};
+</script>
--- src/views/enterprise/AreaSetting.vue
+++ src/views/enterprise/AreaSetting.vue
@@ -1,5 +1,30 @@
 <template>
   <v-container>
+    <v-row align="baseline">
+      <v-spacer></v-spacer>
+      <v-col cols="2">
+        <v-text-field
+          label="区域名称"
+          clearable
+          v-model="searchObj.name"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="2">
+        <v-text-field
+          label="办事处名称"
+          clearable
+          v-model="searchObj.office.name"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn rounded color="primary" dark @click="getObject"> 查询 </v-btn>
+      </v-col>
+      <v-divider vertical></v-divider>
+      <v-col cols="auto">
+        <v-btn rounded color="success" dark> 录入 </v-btn>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
     <v-row>
       <v-col>
         <v-data-table
@@ -10,17 +35,43 @@
           }"
         >
           <template v-slot:[`item.actions`]="{ item }">
-            <v-icon> mdi-delete </v-icon>
+            <v-icon @click="openAreaEdiDialog(item)"> mdi-pencil </v-icon>
+            <v-icon @click="openAreaDelDialog(item.ID)"> mdi-delete </v-icon>
           </template>
         </v-data-table>
       </v-col>
     </v-row>
+
+    <v-dialog v-model="ediDialog" max-width="500px" persistent>
+      <areaForms
+        :closeDialog="closeAreaEdiDialog"
+        :openType="2"
+        :parentObj="ediObj"
+      />
+    </v-dialog>
+
+    <v-dialog v-model="delDialog" max-width="500px" persistent>
+      <v-card>
+        <v-card-title class="text-h5">您确定要删除该区域吗?</v-card-title>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" text @click="deleteArea">确定</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="closeAreaDelDialog">取消</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
-import { queryAreas } from "@/api/oadrp";
+import { queryAreas, delArea } from "@/api/oadrp";
+import areaForms from "@/components/area/Forms";
 export default {
+  components: {
+    areaForms,
+  },
   data: () => ({
     headers: [
       {
@@ -37,15 +88,47 @@ export default {
       },
       { text: "操作", align: "center", value: "actions", sortable: false },
     ],
+    searchObj: {
+      name: "",
+      office: {
+        name: "",
+      },
+    },
     object: [],
+    ediDialog: false,
+    ediObj: {},
+    delDialog: false,
+    deleteID: null,
   }),
   created() {
     this.getObject();
   },
   methods: {
     getObject() {
-      queryAreas().then((res) => {
+      queryAreas(this.searchObj).then((res) => {
         this.object = res.data;
+      });
+    },
+    openAreaEdiDialog(obj) {
+      this.ediObj = obj;
+      this.ediDialog = true;
+    },
+    closeAreaEdiDialog() {
+      this.ediObj = {};
+      this.ediDialog = false;
+    },
+    openAreaDelDialog(id) {
+      this.deleteID = id;
+      this.delDialog = true;
+    },
+    closeAreaDelDialog() {
+      this.deleteID = null;
+      this.delDialog = false;
+    },
+    deleteArea() {
+      delArea(this.deleteID).then((res) => {
+        this.getObject();
+        this.closeAreaDelDialog();
       });
     },
   },
